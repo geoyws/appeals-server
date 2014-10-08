feb1a3c033e42a07241314e3f159192c964130a3
geoyws <geoyws@gmail.com>
2014-10-08 17:19:14 +0800

    did a lot of changes

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 var app = require('express')();
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var routes = require('./routes');
 
 // STARTING SERVER
-var server = app.listen(3000, function () {
-    console.log('Listening on port %d', server.address().port);
-});
+//var server = app.listen(3000, function () {
+//    console.log('Listening on port %d', server.address().port);
+//});
 // END OF STARTING SERVER
 
 // DATABASE CONNECTION INITIALIZATION
 dbURL = 'mongodb://ispaaa:sejahtera@kahana.mongohq.com:10095/app26192063';
 mongoose.connect(dbURL);
 mongoose.connection.on('error', function (err) {
-    console.log('Database connection failed. Error: ' + err);
+  console.log('Database connection failed. Error: ' + err);
 });
 mongoose.connection.once('open', function () {
-    console.log('Successfully connected to database.');
+  console.log('Successfully connected to database.');
 });
 process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-	console.log('Database connection terminated.');
-	process.exit();
-    });
+  mongoose.connection.close(function () {
+    console.log('Database connection terminated.');
+    process.exit();
+  });
 });
 // END OF DATABASE CONNECTION INITIALIZATION
 
 // SCHEMAS
 var ConversationMsgSchema = new Schema({
-    timestamp: Number,
-    geolocationData: {
-	latitude: String,
-	longitude: String,
-	altitude: String,
-	accuracy: String,
-	altitudeAccuracy: String,
-	heading: String,
-	speed: String,
-	timestamp: String
-    },
-    message: {
-	contentType: String,
-	data: String // this will be from S3, we will keep all assets there 
-    }
+  timestamp: Number,
+  geolocationData: {
+    latitude: String,
+    longitude: String,
+    altitude: String,
+    accuracy: String,
+    altitudeAccuracy: String,
+    heading: String,
+    speed: String,
+    timestamp: String
+  },
+  message: {
+    contentType: String,
+    data: String // this will be from S3, we will keep all assets there 
+  }
 });
 // END OF SCHEMAS
-
---
+++ routes/index.js
@@ -0,0 +1,258 @@
+var verbose = process.env.NODE_ENV != 'test';
+var app = module.exports = require('../node_modules/express/lib/express.js')();
+
+app.map = function (a, route) {
+  route = route || '';
+  for (var key in a) {
+    switch (typeof a[key]) {
+      // { '/path': { ... }}
+      case 'object':
+	app.map(a[key], route + key);
+	break;
+      // get: function(){ ... }
+      case 'function':
+	if (verbose) console.log('%s %s', key, route);
+	app[key](route, a[key]);
+	break;
+    }
+  }
+};
+
+// API to be used by routeMap
+var api = {
+  e: { // entities
+    get: function (req, res) { // lists all companies and institutions (entities), admin use only
+    
+    },
+    post: function (req, res) { // allows for new creation of entities and  bulk creations of entities, admin use only
+      
+    },
+    put: function (req, res) { // allows for bulk amendments of entities, admin use only
+      
+    },
+    delete: function (req, res) { // allows for bulk deletions of entities, admin use only, with two-factor auth
+      
+    }
+    eid: {
+      get: function (req, res) { // gets the entity's profile
+
+      },
+      post: function (req, res) { // RESERVED
+
+      },
+      put: function (req, res) { // amends the entity's profile
+
+      },
+      delete: function (req, res) {  // deletes the entity's profile
+
+      },
+      u: { // users
+	get: function (req, res) { // gets a list of all users, only available to admins
+	  res.send('IT WORKS!');
+	},
+	post: function (req, res) { // used for bulk creation of users, available to admins only
+	  res.send('IT WORKS!');
+	},
+	put: function () { // used for bulk amendment of users, available to admins only
+	  res.send('IT WORKS!');
+	},
+	delete: function () { // DANGER: deletes all users, only available to admins, must go through a password auth, used for server cleaning
+	  res.send('IT WORKS!');
+	}, 
+	uid: { // user id
+	  get: function (req, res) { // fetches the profile of the user
+	    res.send('Fetching User ID: %d', req.params.uid); 
+	  },
+	  post: function () { // RESERVED
+	    res.send('IT WORKS!'); 
+	  },
+	  put: function () { // amending user profile, remember to add in the ability to change user roles
+	    res.send('IT WORKS!');
+	  },
+	  delete: function () { // DANGER: require 2 factor auth before deletion of user
+	    res.send('IT WORKS!');
+	  },
+	  c: { // conversations
+	    post: function () { // starts a new conversation
+	    // includes a JSON body that has a { 'participants': 'uid30495, uid204932, uid9192, uid39482' }
+	      res.send('IT WORKS!');
+	    },
+	    active: {
+	      get: function () { // fetches all headers of active conversations, paginated
+		res.send('IT WORKS!');
+	      }
+	    },
+	    archived: {
+	      get: function () { // fetches all headers of archived conversations, paginated
+		res.send('IT WORKS!');
+	      }
+	    },
+	    cid: { // conversation id
+	      get: function () { // fetches individual conversation
+		res.send('IT WORKS!');
+	      },
+	      post: function () { // post a message to this conversation
+	      // cannot exceed 5 posts per second on server and client && messages will be put on a 1s setTimeOut to be sent out
+	      // includes token auth header
+	      // sender gets checked to see if valid member of conversation
+		res.send('IT WORKS!');
+	      },
+	      put: function () { // used for edited messages and deleted messages, users can only edit 5 messages per 30 minutes. All changes will be logged.
+	      // deleted messages 'actionType': 'deleteMsg' will appear with string e.g. 'Message was deleted by George Yong'
+	      // participants can also be added and removed by sending a 'actionType': 'addParticipant' in JSON
+	      // valid rightsholders can also amend conversation options by sending a 'actionType': 'amendOptions' in JSON 
+	      // participants can also resurrect archived conversations by sending a message. The archived conversation immmediately becomes active again.
+		res.send('IT WORKS!');
+	      },
+	      delete: function () { // deletion of conversations are not allowed and conversation will only be sent to archive
+		res.send('IT WORKS!');
+	      }
+	    }
+	  },
+	  watchlist: {
+	    get: function () { // get watchlist of the user
+	      res.send('IT WORKS!');
+	    },
+	    post: function () { // add an appeal to user's watchlist
+	      res.send('IT WORKS!');
+	    },
+	    put: function () { // amend sorting, remove an appeal from the user's watchlist
+	      res.send('IT WORKS!');
+	    },
+	    delete: function () { // clear watchlist, requiring confirmation
+	      res.send('IT WORKS!');
+	    }
+	  },
+	  lists: {
+	    get: function () { // get the lists that the user currently has
+	      res.send('IT WORKS!');   
+	    },
+	    post: function () { // add a new list, either by starting a new one, or copying another list, depending on the JSON argument
+	      res.send('IT WORKS!');
+	    },
+	    put: function () { // amend a list, add appeals and remove appeals from a list, and change the arrangement of a list
+	    // different types of JSON arguments here: add appeal, remove appeal, choose arrangement, etc
+	      res.send('IT WORKS!');
+	    },
+	    delete: function () { // delete a list, or a selection of lists, no looping but one http request to complete multiple deletions
+	      res.send('IT WORKS!');    
+	    }
+	  }
+	}
+      },
+      appeals: {
+	get: function () { // get all appeals paginated by 50 amounts per page, this is specifically for admin
+	  res.send('IT WORKS!');   
+	},
+	post: function () { // mass posting of appeals by admin, must fill in all details needed via JSON in http body
+	  res.send('IT WORKS!');   
+	},
+	put: function () { // mass editing of appeals by admin, must fill in all details needed via JSON in http body
+	  res.send('IT WORKS!');   
+	},
+	delete: function () { // DANGER: delete all appeals, this has 3 factor auth and is only acccessible to admin
+	  res.send('IT WORKS!');    
+	},
+	active: {
+	  get: function () { // this is basically the requesting user's own appeals
+	    res.send('IT WORKS!');
+	  },
+	  post: function () { // post a new appeal, can also post a private appeal
+	    res.send('IT WORKS!');
+	  },
+	  put: function () { // edit the arrangement appearance of the appeals
+	    res.send('IT WORKS!');		
+	  },
+	  delete: function () { // delete/(cancel and archive)  all appeals, requires two factor auth. If an officer has already attended to an appeal, it cannot be deleted
+	    res.send('IT WORKS!');	
+	  }
+	},
+	archived: {
+	  get: function () { // gets paginated 50 per page of user's own archived appeals
+	    res.send('IT WORKS!');	
+	  }
+	},
+	aid: { // appeals id
+	  get: function () { // getting the single appeal by id
+	    res.send('IT WORKS!');	
+	  },
+	  post: function () { // posting like/comment/watch, the api route will accept JSON stating the type (like, comment or watch)
+	    res.send('IT WORKS!');	
+	  },
+	  put: function () { // amend the appeals, title, contents, location, etc.
+	    res.send('IT WORKS!');		
+	  },
+	  delete: function () { // delete the appeal or archive the appeal. After a officer has attended to the appeal, user cannot delete the appeal, but only archive
+	    res.send('IT WORKS!');	
+	  }
+	},
+	nearby: {
+	  get: function () { // get according to the location of the requesting user
+	    res.send('IT WORKS!'); 
+	  }
+	},
+	search: {
+	  searchString: { // search string could be a string or could be an id, dynamic searching supporting multiple searches within one string and different sorting
+	    get: function () {
+	      res.send('IT WORKS!');
+	    }
+	  }	
+	}
+      }
+    }
+  }
+};
+
+
+// Route Map
+app.map({
+    '/u': { // users
+	get: api.e.eid.u.get, // for getting a basic list of users, which is only for admins
+	post: api.e.eid.u.post, // admin use, batch adding users, and for the system to add new users
+	put: api.e.eid.u.put, // admin use, batch editing users
+	delete: api.e.eid.users.delete, // admin use, batch deleting users, JSON arguments
+	'/:uid': { // user IDs
+	    get: api.e.eid.u.uid.get, // for getting all profile info
+	    post: api.e.eid.u.uid.post, // NOT IN USE, reserved
+	    put: api.e.eid.u.uid.put, // editing user profile
+	    delete: api.e.eid.u.uid.delete, // for deleting the user profile, use with caution and layer with security
+	    '/c': { // conversations
+		get: api.e.eid.u.uid.get, // for getting all conversations, active and archived are only denoted by the conversation's property in the db
+		post: api.e.eid.u.uid.post, // for starting a new conversation
+		put: api.e.eid.u.uid.put, // for editing conversations in bulk, whatever it may be
+		delete: api.e.eid.u.uid.delete // for deleting conversations in bulk, two step auth
+		'/:cid': { // conversation id
+		    get: api.users.uid.conversations.cid.get, // get the latest 50 messages, this is different from push systems
+		    post: api.users.uid.conversations.cid.post, // post a msg to conversation, with token auth, checks if poster is valid participant, resurrect archived conversation
+		    put: api.users.uid.conversations.cid.put, // edits and deletes posts, add and remove participants, change conversation options
+		    delete: api.users.uid.conversations.cid.delete, // archives the conversation and it no longer is active
+		}
+	    },
+	    '/w': { // watchlists
+		get: api.users.uid.watchlist.get,
+		post: api.users.uid.watchlist.post,
+		put: api.users.uid.watchlist.put,
+		delete: api.users.uid.watchlist.delete
+	    },
+	    '/l': { // lists
+		get: api.users.uid.lists.get,
+		post: api.users.uid.lists.post,
+		put: api.users.uid.lists.put,
+		delete: api.users.uid.lists.delete
+	    }
+	}
+    },
+    '/appeals': {
+	'/search': {
+	    '/:searchString': {
+		get: api.appeals.search.searchString.get
+	    }
+	}
+    }
+});
+
+var server = app.listen(3000, function () {
+  console.log('Listening on port %d', server.address().port);
+});
+
+//module.exports.api = api;
